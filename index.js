c8bc8503d15d20f2e321e3353acfae825e01a3bf
MurilloSSJ <murillostore@gmail.com>
2022-03-11 19:01:51 -0300

    Criação do jogo

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 import {Snake} from './snake.js'
 import {Apple} from './apple.js'
 const canvas = document.querySelector("#canvas")
+canvas.width = `${window.innerWidth}`
+canvas.height = `${window.innerHeight - 4}`
 var snake = new Snake()
 var apple = new Apple(snake)
 //var coin = new Apple();
 var canvasContext = canvas.getContext('2d')
-
 window.onload = () =>{
     loop()
 }
-function loop(){
-    setInterval(show, 1000/10)
+window.onresize = ()=>{
+    canvas.width = `${window.innerWidth}`
+    canvas.height = `${window.innerHeight - 4}`
+    apple = new Apple(snake)
 }
-
+function loop(){
+    setInterval(show, 1000/20)
+}
 function show(){
     snake.move()
+    if(snake.gameOver()){
+        alert("game over")
+        snake = new Snake()
+    }
     eatCoin()
     //Inicializando tela
     componentRender(0,0,canvas.width,canvas.height,'black')
     //Inicializando score
     canvasContext.font = '50px Arial'
     canvasContext.fillStyle = '#00FF42'
-    canvasContext.fillText("Score: *",snake.size,canvas.width-120,50)
+    canvasContext.fillText(`Score:${snake.tail.length} `,canvas.width - 120,120,50)
     //Inicializando a cobra
     componentRender(apple.x,apple.y,apple.size,apple.size,apple.color)
     for(let i=0;i< snake.tail.length; i++){
-        componentRender(snake.tail[i].x +2.5,snake.tail[0].y +2.5,snake.size -5,snake.size -5,'white')
+        componentRender(snake.tail[i].x,snake.tail[i].y,snake.size -5,snake.size -5,'white')
     }
 }
 function componentRender(x,y,width,height,color){
@@ -36,10 +45,10 @@ function componentRender(x,y,width,height,color){
 //Evento de andar
 window.addEventListener('keydown',(event)=>{
     if(event.code == 'ArrowUp'){
-        snake.rotateY = -1
+        snake.rotateY = 1
         snake.rotateX = 0
     }else if(event.code == 'ArrowDown'){
-        snake.rotateY = 1
+        snake.rotateY = -1
         snake.rotateX = 0
     }else if(event.code == 'ArrowLeft'){
         snake.rotateX = -1
@@ -53,12 +62,9 @@ window.addEventListener('keydown',(event)=>{
 //Comer maçã
 function eatCoin(){
     for(let i=0;i<snake.tail.length;i++){
-        if(Math.abs(snake.tail[i].x - apple.x <=snake.size) && Math.abs(snake.tail[i].y - apple.y <= snake.size)){
-            snake.tail[snake.tail.length] = {x:apple.x, y: apple.y}
+        if(Math.abs(snake.tail[i].x - apple.x) <= snake.size && Math.abs(snake.tail[i].y - apple.y) <= snake.size){
+            snake.eat()
             apple = new Apple(snake) 
         }
     }
 }
-//function gameLoop(){
-  //  setInterval(show,1000/15) //15 fps
-//}
--- snake.js
+++ snake.js
@@ -11,21 +11,44 @@ class Snake{
     }
     move(){
         let newRect
-        if (this.rotateX != 0){
-            let xPosition = this.rotateX == 1 ? this.tail[this.tail.length -1].x + this.size : this.tail[this.tail.length -1].x - this.size
+        if(this.tail[0].x > canvas.width){
             newRect = {
-                x: xPosition,
-                y: this.tail[this.tail.length -1].y
-            }
-        }else{
-            let yPosition = this.rotateY == 1 ? this.tail[this.tail.length -1].y + this.size : this.tail[this.tail.length -1].y - this.size
-            newRect = {
-                y:yPosition,
-                x:this.tail[this.tail.length -1].x
+                x: this.tail[0].x - canvas.width,
+                y: this.tail[0].y
             }
         }
-        this.tail.shift()
-        this.tail.push(newRect)
+        else if(this.tail[0].x < 0){
+            newRect = {
+                x: canvas.width - Math.abs(this.tail[0].x),
+                y: this.tail[0].y
+            }
+        }
+        else if(this.tail[0].y>canvas.height){
+            newRect = {
+                x: this.tail[0].x,
+                y: this.tail[0].y - canvas.height
+            }
+        }else if(this.tail[0].y < 0){
+            newRect = {
+                x: this.tail[0].x,
+                y: canvas.height - Math.abs(this.tail[0].y)
+            }
+        }
+        else if (this.rotateX != 0){
+            let xPosition = this.rotateX == 1 ? this.tail[0].x + this.size : this.tail[0].x - this.size
+            newRect = {
+                x: xPosition,
+                y: this.tail[0].y
+            }
+        }else{
+            let yPosition = this.rotateY == 1 ? this.tail[0].y - this.size : this.tail[0].y + this.size
+            newRect = {
+                y:yPosition,
+                x:this.tail[0].x
+            }
+        }
+        this.tail.pop()
+        this.tail.unshift(newRect)
     }
     eat(){
         let newRect
@@ -33,16 +56,28 @@ class Snake{
             let xPosition = this.rotateX == 1 ? this.tail[this.tail.length -1].x + this.size : this.tail[this.tail.length -1].x - this.size
             newRect = {
                 x: xPosition,
-                y: this.tail[this.tail.length -1].y
+                y: this.tail[0].y
             }
         }else{
-            let yPosition = this.rotateY == 1 ? this.tail[this.tail.length -1].y + this.size : this.tail[this.tail.length -1].y - this.size
+            let yPosition = this.rotateY == 1 ? this.tail[0].y - this.size : this.tail[0].y + this.size
             newRect = {
                 y:yPosition,
-                x:this.tail[this.tail.length -1].x
+                x:this.tail[0].x
             }
         }
         this.tail.push(newRect)
+    }
+    gameOver(){
+        if (this.tail.length > 1){
+            var newTail = this.tail[0]
+            for(let i = 1;i < this.tail.length;i++){
+                if(newTail.x == this.tail[i].x && newTail.y == this.tail[i].y){
+                    return true
+                }
+            }
+            return false
+        }
+        return false
     }
 }
 export {Snake}
